a869a0c99ba0dbed7da3f10ea06fb1508838b6f1
samuelribeiroo <samuelribeirodev@gmail.com>
2024-06-11 20:06:34 -0300

    refactor: single responsability to render image and info

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ const product = products.productList;
 
 const productID = product.map(({ id }) => id);
 
-const productName = product.map(({ name }) => name);
-
-const productPrice = product.map(({ price }) => price);
+const productAmount = product.map(({ amount }) => amount);
 
 const productImage = product.map(({ image }) => image);
 
 const mainContainer = document.querySelector("main");
 
 const productContainer = document.querySelector(".product-container");
-
-const productDetails = document.querySelector(".product-detail");
 
 const formatCurrency = price => {
   let format = new Intl.NumberFormat("pt-BR", {
@@ -39,29 +35,59 @@ cartCloseButton.addEventListener("click", () => {
   mainContainer.style.display = "none";
 });
 
-function showCart() {
-  productImage.forEach((img, index) => {
-    const image = document.createElement("img");
-    image.style.width = "104px";
-    image.style.height = "104px";
-    image.alt = `ID DO PRODUTO: ${productID[index]}`;
-    image.classList.add("product-image");
-    image.src = img;
+const displayProductImage = productImage.forEach((img, index) => {
+  const image = document.createElement("img");
+  image.src = img;
+  image.style.width = "104px";
+  image.style.height = "104px";
+  image.alt = `ID DO PRODUTO: ${productID[index]}`;
+  image.classList.add("product-image");
+  image.fetchPriority = "high";
+  productContainer.appendChild(image);
+});
 
-    productContainer.appendChild(image);
-  });
+const displayProductInfo = product.forEach(({ name, price }, index) => {
+  const productTitle = document.createElement("h1");
+  productTitle.classList.add("product-name");
+  productTitle.textContent = name;
+  productContainer.appendChild(productTitle);
 
-  product.forEach(({ name, price }) => {
-    const productTitle = document.createElement("h1");
-    productTitle.classList.add("product-name");
-    productTitle.textContent = name;
-    productDetails.appendChild(productTitle);
+  const productDetailsGroup = document.createElement("div");
+  productDetailsGroup.classList.add("product-details-group");
 
-    const priceTag = document.createElement("p");
-    priceTag.classList.add("product-price");
-    priceTag.textContent = `${formatCurrency(price)}`;
-    productDetails.appendChild(priceTag);
-  });
-}
+  const handleChangeAmountContainer = document.createElement("div");
+  handleChangeAmountContainer.classList.add("increase-or-decrease");
+
+  const decreaseButton = document.createElement("button");
+  decreaseButton.id = "minus";
+  decreaseButton.disabled = true;
+  decreaseButton.innerHTML =
+    '<img src="./assets/Minus.svg" alt="Decrease Quantity" fetchpriority="high" aria-label="Diminuir Quantidade">';
+  handleChangeAmountContainer.appendChild(decreaseButton);
+
+  const displayAmount = document.createElement("p");
+  displayAmount.textContent = `${productAmount[index]}`;
+  handleChangeAmountContainer.appendChild(displayAmount);
+
+  const increaseButton = document.createElement("button");
+  increaseButton.id = "plus";
+  increaseButton.innerHTML =
+    '<img src="./assets/Plus.svg" alt="Increase Quantity" fetchpriority="high" aria-label="Aumentar Quantidade">';
+  increaseButton.style.cursor = "pointer";
+  handleChangeAmountContainer.appendChild(increaseButton);
+
+  const priceTag = document.createElement("p");
+  priceTag.classList.add("product-price");
+  priceTag.textContent = `${formatCurrency(price)}`;
+  productContainer.appendChild(priceTag);
+
+  productDetailsGroup.appendChild(priceTag);
+  productDetailsGroup.appendChild(handleChangeAmountContainer);
+  productContainer.appendChild(productDetailsGroup);
+});
+
+const showCart = () => {
+  displayProductImage, displayProductInfo;
+};
 
 showCart();
