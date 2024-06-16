db6c1bf566e0dce26d22186ad45088d29ed9f4d6
samuelribeiroo <samuelribeirodev@gmail.com>
2024-06-16 18:45:44 -0300

    update: JSON with more items

--- utils/products.js
+++ utils/products.js
@@ -55,4 +55,48 @@ export const productList = [
     image:
       "https://firebasestorage.googleapis.com/v0/b/second-project-15bba.appspot.com/o/goku-action-figura.jpg?alt=media&token=4fb22ba5-7379-40ce-afdf-8e3928037420",
   },
+  {
+    id: 8,
+    amount: 4,
+    name: "Monitor Gamer Corsair Xeneon 32, 144Hz, 4k, 1ms, IPS, DisplayPort e HDMI, HDR, FreeSync",
+    price: 6899.99,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/second-project-15bba.appspot.com/o/monitor-free-sync.jpg?alt=media&token=403f660f-d9d9-4643-a0f3-859d36b546aa",
+  },
+
+  {
+    id: 9,
+    amount: 6,
+    name: "Ventoinha Rise Mode Wind W1, 120mm, LED Branco, Preto - RM-WN-01-BW",
+    price: 7.99,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/second-project-15bba.appspot.com/o/cooler-fan-rise-mode.jpg?alt=media&token=9a2d47d6-96fb-41b0-86c1-35ab955cab4d",
+  },
+
+  {
+    id: 10,
+    amount: 4,
+    name: "Fonte Corsair CV550, 550W, 80 Plus Bronze, PFC Ativo, com cabo, Preto - CP-9020210-BR",
+    price: 279.99,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/second-project-15bba.appspot.com/o/fonte-corsair-cv550.jpg?alt=media&token=7f38a9ef-b690-438e-9e91-8a6eeb4c8993",
+  },
+
+  {
+    id: 11,
+    amount: 2,
+    name: "SSD SATA Kingston, A400, 480GB, 2.5, Leitura: 500MB/s e Gravação: 450MB/s, Preto - SA400S37/480G",
+    price: 229.99,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/second-project-15bba.appspot.com/o/sdd-kingston.jpg?alt=media&token=10947d71-c874-450a-a4e4-11230742f148",
+  },
+
+  {
+    id: 12,
+    amount: 3,
+    name: "Roteador Intelbras RX1500, 802.11ax, Wi-Fi 6, 1500Mbps, Dual Band, 4 Antenas - 4750128",
+    price: 214.99,
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/second-project-15bba.appspot.com/o/roteador-wireless.jpg?alt=media&token=f2b30063-60bb-4e85-9bd0-41e0a9b6cde5",
+  },
 ];
